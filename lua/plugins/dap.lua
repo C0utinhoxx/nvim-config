29c43e0dc72b5db4a442bf28f1408582ce5df39f
C0utinhoxx <brunoriquelme.coutinho@gmail.com>
2026-05-06 19:53:21 -0300

    Configure icons

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
+
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      -- abrir/fechar UI automaticamente
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- atalhos
+      vim.keymap.set("n", "<F5>", dap.continue)
+      vim.keymap.set("n", "<F10>", dap.step_over)
+      vim.keymap.set("n", "<F11>", dap.step_into)
+      vim.keymap.set("n", "<F12>", dap.step_out)
+      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+    end,
+  },
+}
---
+++ lua/plugins/icons.lua
@@ -0,0 +1,78 @@
+return {
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+
+    opts = {
+      default = true,
+
+      override_by_extension = {
+        js = {
+          icon = "",
+          color = "#f1e05a",
+          name = "Js",
+        },
+
+        ts = {
+          icon = "",
+          color = "#519aba",
+          name = "Ts",
+        },
+
+        jsx = {
+          icon = "",
+          color = "#20c2e3",
+          name = "Jsx",
+        },
+
+        tsx = {
+          icon = "",
+          color = "#1354bf",
+          name = "Tsx",
+        },
+
+        json = {
+          icon = "",
+          color = "#cbcb41",
+          name = "Json",
+        },
+
+        lua = {
+          icon = "",
+          color = "#51a0cf",
+          name = "Lua",
+        },
+
+        php = {
+          icon = "",
+          color = "#a074c4",
+          name = "Php",
+        },
+
+        html = {
+          icon = "",
+          color = "#e34c26",
+          name = "Html",
+        },
+
+        css = {
+          icon = "",
+          color = "#563d7c",
+          name = "Css",
+        },
+
+        md = {
+          icon = "",
+          color = "#519aba",
+          name = "Markdown",
+        },
+
+        env = {
+          icon = "",
+          color = "#faf743",
+          name = "Env",
+        },
+      },
+    },
+  },
+}
